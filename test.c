d116dec818a0c4d38a08d2a92a5ad1d40dc4a55a
nadaf-reshma <49471505+nadaf-reshma@users.noreply.github.com>
2019-04-10 14:24:42 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter a and b value:");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+
+    printf("Sum=%d",sum)
+    return 0;
+}
